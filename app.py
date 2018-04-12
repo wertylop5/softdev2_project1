b2b977d6e1210f29e1ccadb3f70345b27ad86446
Stanley Lin <slin55055@gmail.com>
2018-04-12 14:20:01 -0400

    display template

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+import csv
 
 app = Flask(__name__)
 
 @app.route("/")
 def root():
-	return "Hello"
+	return render_template("base.html")
+
+@app.route("/data", methods=["POST"])
+def data():
+	res = ""
+	with open("data/traffic-data.csv") as data_file:
+		reader = csv.reader(data_file)
+		for row in reader:
+			print row
+	return ""
 
 if __name__ == "__main__":
 	app.debug = True
